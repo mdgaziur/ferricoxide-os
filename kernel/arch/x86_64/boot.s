22a2103ff8cee247ac11aba8f5d850d482c79587
mdgaziur <mdgaziurrahmannoor@gmail.com>
2023-10-13 02:43:24 +0600

    Paging: enable recursive paging

--- kernel/arch/x86_64/boot.s
+++ kernel/arch/x86_64/boot.s
@@ -74,6 +74,10 @@ long_mode_check:
 
 set_up_page_tables:
     ; map first P4 entry to P3 table
+    mov eax, p4_table
+    or eax, 0b11
+    mov [p4_table + 511 * 8], eax
+
     mov eax, p3_table
     or eax, 0b11
     mov [p4_table], eax
