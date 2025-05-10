df176ebcf658911c10723b2d72b9a8b0da6dd0bb
mdgaziur <mdgaziurrahmannoor@gmail.com>
2025-05-10 15:02:01 +0600

    Kernel: increase double fault handler stack size to 4 KB

--- kernel/arch/x86_64/gdt.rs
+++ kernel/arch/x86_64/gdt.rs
@@ -27,7 +27,7 @@ lazy_static! {
     static ref TSS: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
-            const STACK_SIZE: usize = 1 * KB;
+            const STACK_SIZE: usize = 4 * KB;
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
             let stack_top = VirtAddr::from_ptr(&raw const STACK);
